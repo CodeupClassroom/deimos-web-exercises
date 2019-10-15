aa8ea27e1d8ac60385912038449d9353c463c3a2
sophiakurihara <sophia.sudiacal@gmail.com>
2019-10-15 14:34:56 -0500

    loops lecture

---
+++ js/loops-lec.js
@@ -0,0 +1,111 @@
+"use strict";
+/*
+* WHILE LOOPS
+*/
+
+//initialization
+var i = 10;
+
+while(i > 0){
+    console.log(i);
+    i--;
+}
+console.log("Loop is done");
+
+// 1. Create a while loop that counts to 100 in increments of ten:
+// (10...20...30...)
+
+// while(i <= 90){
+//     console.log(i+=10);
+// }
+
+// 2. Create a while loop that counts backwards from 50 - 1
+
+var x = 50;
+
+while(x >= 1){
+    console.log(x);
+    x--;
+}
+
+var myQuestion;
+
+// 3. Create a while loop that asks the user for a confirmation when asked: "Would you like to exit?"
+// Store the confirm in the myQuestion variable.
+// Try running this. What happens?
+
+// while(!myQuestion){
+//     myQuestion = confirm("Would you like to exit?");
+// }
+
+
+/*
+* DO-WHILE LOOPS
+*/
+
+
+// 4. Refactor question 3 using a do-while loop.
+// What is the behavior this time?
+
+do{
+    myQuestion = confirm("Would you like to exit?");
+}while(!myQuestion);
+
+// 5. Create a do-while loop that prompts the user for a color and continues
+// to prompt until the string passed is "blue"
+
+do{
+    var myColor = prompt("Your favorite color?");
+}while(myColor !== "blue");
+
+// 6. Refactor question 1 using a do-while loop.
+
+do{
+    console.log(i+=10);
+}while(i < 100);
+
+/*
+* FOR LOOPS
+*/
+
+function countFrom(x){
+    for(var j = x;j <= 100;j++){
+        console.log(j);
+    }
+    return "Done counting!";
+}
+
+var randomColors = ["red","orange","yellow","blue","green"];
+
+    for(var y = 0;y < randomColors.length;y++){
+        if(y % 2 === 0){
+            console.log(randomColors[y]);
+        }
+    }
+
+var myArray = ["apples", "oranges", "bananas", "grapes", "pineapples"];
+
+// 7. Create a for loop that iterates over this array and logs out each
+// element to the console
+
+
+// 8. You are going to use a for loop to complete the FizzBuzz exercise.
+// Your program should count up to 100.
+// For numbers divisible by 3, log "Fizz"
+// For numbers divisible by 5, log "Buzz"
+// If divisible by 3 and 5, log "FizzBuzz"
+// All other cases, just log the number.
+
+// 9. Refactor question 1 using a for-loop
+
+
+
+/*
+* BREAK AND CONTINUE
+*/
+
+// 11. Refactor question 1: using a 'break', stop the counting once it hits 70.
+
+// 12. Refactor question 2: using a 'continue', skip numbers 50-26 and only
+// log 25 - 1.
+
