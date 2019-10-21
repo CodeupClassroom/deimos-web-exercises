b69d0acc0a4e582fa984738c48d5afc472a31710
sophiakurihara <sophia.sudiacal@gmail.com>
2019-10-21 09:13:31 -0500

    morning warmup done

--- js/morning-warmups.js
+++ js/morning-warmups.js
@@ -1,5 +1,12 @@
 // "use strict";
 
+function countUp(x){
+    for(var i = 1; i <= x; i++){
+        console.log(i);
+    }
+    return "Done counting!";
+}
+
 // Write a JavaScript function that takes in floating point number, and returns the number rounded down. It should return false for all inputs that aren’t numbers.
 
 function roundDownFloat(num) {
@@ -36,9 +43,9 @@ console.log(quotient("21",7), false);
 
 // Write a JavaScript function returns the message, “Hello there, stranger.” Alert the return from this function. Refactor so that it returns “Hello there, “ concatenated to an argument. Prompt the user to input a string, call the function, and alert the return. Refactor so that the function has ‘stranger’ as a default value for the parameter.
 
-function helloThere(string = "stranger") {
-    return 'Hello there, ' + string + '.';
-}
+// function helloThere(string = "stranger") {
+//     return 'Hello there, ' + string + '.';
+// }
 // var promptedResponse = prompt('Input string, plox.');
 // alert(helloThere(promptedResponse));
 
